b2b5edf07def1c382109cf6b6c2b18adc486184b
the-prism <t.castonguay.gagnon@gmail.com>
2017-08-04 16:12:33 -0400

    init project vs

---
+++ Common/DragonLib/Entities/Player.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragonLib.Entities
+{
+    public class Player
+    {
+    }
+}
---
+++ Common/DragonLib/Environnement/Board.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DragonLib.Environnement
+{
+    public class Board
+    {
+    }
+}
