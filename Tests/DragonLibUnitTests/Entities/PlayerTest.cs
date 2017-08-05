37bc3260cb7cc87d08beb965a4b45a4db6ed3932
the-prism <t.castonguay.gagnon@gmail.com>
2017-08-05 14:36:36 -0400

    test clases

---
+++ Tests/DragonLibUnitTests/Entities/PlayerTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DragonLibUnitTests.Entities
+{
+    [TestClass]
+    public class PlayerTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ Tests/DragonLibUnitTests/Environnement/BoardTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DragonLibUnitTests.Environnement
+{
+    [TestClass]
+    public class BoardTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
