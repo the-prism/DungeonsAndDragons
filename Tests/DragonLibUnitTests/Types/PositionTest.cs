1b49a5f3c1f18f5998d1de81e2a1680b9693d3cf
the-prism <t.castonguay.gagnon@gmail.com>
2017-08-09 19:39:29 -0400

    position test

---
+++ Tests/DragonLibUnitTests/Types/PositionTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DragonLib.Types;
+
+namespace DragonLibUnitTests.Types
+{
+    [TestClass]
+    public class PositionTest
+    {
+        [TestMethod]
+        public void TestPositionConstructorX()
+        {
+            Position position = new Position(34, 0);
+            Assert.AreEqual(34, position.PositionX);
+        }
+
+        [TestMethod]
+        public void TestPositionConstructorY()
+        {
+            Position position = new Position(0, 23);
+            Assert.AreEqual(23, position.PositionY);
+        }
+
+        [TestMethod]
+        public void TestPositionChangeX()
+        {
+            Position position = new Position(0, 23);
+            position.ChangePosition(10, 23);
+            Assert.AreNotEqual(0, position.PositionX);
+            Assert.AreEqual(10, position.PositionX);
+        }
+
+        [TestMethod]
+        public void TestPositionChangeY()
+        {
+            Position position = new Position(0, 0);
+            position.ChangePosition(0, 23);
+            Assert.AreNotEqual(0, position.PositionY);
+            Assert.AreEqual(23, position.PositionY);
+        }
+    }
+}
