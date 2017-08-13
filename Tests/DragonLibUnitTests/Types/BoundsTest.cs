74e5883dfea232c930d56ac20bbc3b726a6a2886
the-prism <t.castonguay.gagnon@gmail.com>
2017-08-12 23:12:30 -0400

    moar tests

--- Tests/DragonLibUnitTests/Types/BoundsTest.cs
+++ Tests/DragonLibUnitTests/Types/BoundsTest.cs
@@ -20,7 +20,54 @@ namespace DragonLibUnitTests.Types
         [TestMethod]
         public void TestBoundsFrom2()
         {
+            Bounds limit = new Bounds(2, 6, 2, 3);
+            Assert.AreEqual(2, limit.MinX);
+            Assert.AreEqual(2, limit.MinY);
+            Assert.AreEqual(6, limit.MaxX);
+            Assert.AreEqual(3, limit.MaxY);
+        }
 
+        [TestMethod]
+        public void TestExpandBounds()
+        {
+            Bounds limit = new Bounds(2, 6, 2, 3);
+            Assert.AreEqual(2, limit.MinX);
+            limit.SetXBoudaries(0, 6);
+            Assert.AreEqual(0, limit.MinX);
+        }
+
+        [TestMethod]
+        public void TestIsInsideBounds()
+        {
+            Bounds limit = new Bounds(-2, 2, -2, 2);
+            Position position = new Position(0, 0);
+            bool result = limit.IsInsideBounds(position);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestIsInsideBoundsX()
+        {
+            Bounds limit = new Bounds(-2, 2, -2, 2);
+            bool result = limit.IsInsideXBounds(1);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestIsInsideBoundsY()
+        {
+            Bounds limit = new Bounds(-2, 2, -2, 2);
+            bool result = limit.IsInsideXBounds(-2);
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void TestIsOutsideBounds()
+        {
+            Bounds limit = new Bounds(-2, 2, -2, 2);
+            Position position = new Position(0, 3);
+            bool result = limit.IsInsideBounds(position);
+            Assert.AreEqual(false, result);
         }
     }
 }
