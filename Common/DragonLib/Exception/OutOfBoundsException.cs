6ba92dcbd975f45b77b7f04e57b19cc455aa0bdc
the-prism <t.castonguay.gagnon@gmail.com>
2017-09-07 11:47:52 -0400

    Execption methods

--- Common/DragonLib/Exception/OutOfBoundsException.cs
+++ Common/DragonLib/Exception/OutOfBoundsException.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace DragonLib
 {
     public class OutOfBoundsException : Exception
     {
+        public OutOfBoundsException()
+        {
+        }
+
+        public OutOfBoundsException(string message) : base(message)
+        {
+        }
+
+        public OutOfBoundsException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected OutOfBoundsException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
     }
 }
